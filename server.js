5617921c7fec962d1157deda4c96539030d75a0a
vdodwad111-hue <vdodwad111@gmail.com>
2026-09-09 17:06:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("JARVIS AI is online 🤖");
-});
+app.use(express.static("public"));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`JARVIS running on port ${PORT}`);
